ab84ce4270defd492756d0a38d3025c64b712bb4
John Morrissey <jwm@horde.net>
2013-02-17 18:00:29 -0500

    make the docs build process actually, you know, work.

--- build.sh
+++ build.sh
@@ -1,108 +1,11 @@
-#!/bin/bash
-#
-# Script to manage the building of the userguide
-#
-BASE="`pwd`"
-INSTALL_ROOT="$HOME/Proftpd/"
-##
-## Test to see if we can find a stylesheet using a 
-## provided path
-##
-if [ -f $DSSSL_STYLESHEET_PATH/html/docbook.dsl ]
-then
-  DSL="$DSSSL_STYLESHEET_PATH/html/docbook.dsl"
-else
-  # Fall back to a hardcoded default
-  DSL="/usr/lib/sgml/stylesheet/dsssl/docbook/nwalsh/html/docbook.dsl"
-fi
+#!/bin/sh
 
-function clean
-{
-#
-# Clean up
-#
-cd $BASE
-rm -rf linked
-rm -rf other
-mkdir -p ./linked/
-mkdir -p ./other/
-}
-#
-function build
-{
-	clean
-	echo -n "Building "
-#	if [ ! -f directives/other/directive_list.sgml ]
-#	then
-#		echo "The directive list is missing, aborting"
-#		exit
-#	fi
-	#
-	# Building linked html
-	# 
-	echo -n "linked "
-	cd $BASE/linked/
-	jade -t sgml -E 20 -d $DSL $BASE/main.sgml
-	#
-	# Building single html 
-	#
-	echo -n "html "
-	cd $BASE/other/
-	jade -t sgml -V nochunks -d $DSL \
-		$BASE/main.sgml > userguide_full.html 
-	#	
-	# Building PS & PDF
-	#
-	echo -n "pdf "
-	htmldoc -t pdf userguide_full.html > userguide.pdf
-	echo -n "ps "
-	htmldoc -t ps userguide_full.html > userguide.ps
-	echo "."
-}
+BASE="$(pwd)"
+DSL="/usr/share/sgml/docbook/stylesheet/dsssl/modular/html/docbook.dsl"
 
-function install
-{
-	cd $BASE/linked
-	rsync -av * ~/Proftpd/www.proftpd.org-new/localsite//Userguide/linked
-	cd $BASE/other
-	rsync -av * ~/Proftpd/www.proftpd.org-new/localsite//Userguide/other  
-}
+jade -t sgml -V nochunks -d "$DSL" "$BASE/main.sgml" \
+	>"$BASE/userguide_full.html"
 
-function installbeta {
-	cd $HOME/Proftpd/Userguide/
-	rsync --delete -av linked/ ~/Proftpd/www.proftpd.org/localsite/beta/linked
-	rsync --delete -av other/ ~/Proftpd/www.proftpd.org/localsite/beta/other
-} 
-#
-#
-#
-case "$1" in
-	build)
-  		echo "Building userguide"
-  		build
-		;;
-	install)
-		echo "Installing pages"
-		install
-		;;
-	beta)
-		build
-		installbeta
-		;;
-	installbeta)
-		installbeta
-		;;
-	clean)
-		echo "Nuking built"
-		clean
-      ;;
-	all)
-		build
-		install
-      ;;
-	*)
-		echo "Usage: build.sh (build|clean|install|all)"
-    	exit 1
-		;;
-esac
-#
+cd "$BASE/linked/"
+jade -t sgml -E 20 -d "$DSL" "$BASE/linked/../main.sgml"
+
--- directives/build_output.sh
+++ directives/build_output.sh
@@ -1,31 +1,121 @@
-#!/bin/bash
+#!/bin/sh
 
-ROOT=`pwd`
-OUTPUT=${ROOT}/output/
-SRC="$ROOT/sgml/"
-if [ "$DSSSL_STYLESHEET_PATH" != "" ] 
-then
-  DSL="$DSSSL_STYLESHEET_PATH/html/docbook.dsl"
-else 
-  DSL="/usr/lib/sgml/stylesheet/dsssl/docbook/nwalsh/html/docbook.dsl"
-fi
-WWW=$HOME/Proftpd/www.proftpd.org-new/docs/
-FTP=$HOME/Proftpd/ftp.proftpd.org-new/docs/
+set -e
+ROOT="$(dirname "$0")"
+DSL="/usr/share/sgml/docbook/stylesheet/dsssl/modular/html/docbook.dsl"
 
-$ROOT/build_by_name.sh
-$ROOT/build_by_module.sh
-$ROOT/build_by_context.sh
+directives_for_context()
+{
+	local context=$1
+	shift
 
-mkdir -p $OUTPUT/linked/
-cd $OUTPUT/linked/
-rm -fv *html
-jade -t sgml -E 10000 -d $DSL $OUTPUT/configuration.sgml
+	grep -A3 Context "$ROOT/sgml/"* | fgrep "$context" | \
+		cut -d '-' -f 1 | sed 's,.*/,,'
+}
 
-cd $OUTPUT
-jade -t sgml -V nochunks -E 100000 -d $DSL configuration.sgml > configuration_full.html
+build_by_context()
+{
+	local context directive
 
-htmldoc -t pdf configuration_full.html > configuration.pdf
-htmldoc -t ps configuration_full.html > configuration.ps
+	directives_for_context 'server config' >"$ROOT/output/context-serverconfig"
+	directives_for_context '&lt;Global&gt;' >"$ROOT/output/context-Global"
+	directives_for_context '&lt;VirtualHost&gt;' >"$ROOT/output/context-VirtualHost"
+	directives_for_context '&lt;Anonymous&gt;' >"$ROOT/output/context-Anonymous"
+	directives_for_context '&lt;Limit&gt;' >"$ROOT/output/context-Limit"
+	directives_for_context '.ftpaccess' >"$ROOT/output/context-ftpaccess"
 
-rsync -av --delete $OUTPUT/ $WWW/directives/
-##rsync -av --delete $OUTPUT/ $FTP/directives/
+	rm -f "$ROOT/output/by_context_source.sgml"
+	for context in serverconfig Global VirtualHost Anonymous Limit ftpaccess; do
+		cat "$ROOT/definitions/context/$context" \
+			>>"$ROOT/output/by_context_source.sgml"
+
+		for directive in $(cat "$ROOT/output/context-$context"); do
+			echo "<link linkend=\"$directive\">$directive</link>" \
+				>>"$ROOT/output/context-$context.sgml"
+		done
+
+		echo "</para>" >>"$ROOT/output/by_context_source.sgml"
+		echo "</refsect1>" >>"$ROOT/output/by_context_source.sgml"
+		echo "</refentry>" >>"$ROOT/output/by_context_source.sgml"
+		echo "" >>"$ROOT/output/by_context_source.sgml"
+		echo "" >>"$ROOT/output/by_context_source.sgml"
+	done
+}
+
+module_and_directive_list()
+{
+	local file module directive
+
+	for file in $(find "$ROOT/sgml/" -type f); do
+		module=$(grep -A 2 "<keyword>" "$file" |
+			sed -n '/mod_/s/.*\(mod_[a-z]*\).*/\1/p')
+		directive=${file##*/}
+		echo "$module:$directive"
+	done
+}
+
+build_by_module()
+{
+	local module_and_directive module directive
+	local last_module
+
+	rm -f "$ROOT/output/by_module_source.sgml"
+	# Module name must always be first so the list is sorted
+	# by module in our output.
+	for module_and_directive in $(module_and_directive_list | sort -f); do
+		module=${module_and_directive%%:*}
+		directive=${module_and_directive##*:}
+
+		if [ "$module" != "$last_module" ]; then
+			if [ ! -e "$ROOT/definitions/module/$module" ]; then
+				echo "$module doesn't have a module definition in $ROOT/definitions/module, skipping..." 1>&2
+				continue
+			fi
+
+			cat "$ROOT/definitions/module/$module" \
+				>>"$ROOT/output/by_module_source.sgml"
+			last_module="$module"
+		fi
+
+		echo "<link linkend=\"$directive\">$directive</link>" \
+			>>"$ROOT/output/by_module_source.sgml"
+
+		echo "</para>" >>"$ROOT/output/by_module_source.sgml"
+		echo "</refsect1>" >>"$ROOT/output/by_module_source.sgml"
+		echo "</refentry>" >>"$ROOT/output/by_module_source.sgml"
+		echo "" >>"$ROOT/output/by_module_source.sgml"
+		echo "" >>"$ROOT/output/by_module_source.sgml"
+	done
+}
+
+build_by_name()
+{
+	local file directive
+
+	cat "$ROOT/sgml/"* >>"$ROOT/output/by_name_source.sgml"
+
+	rm -f "$ROOT/output/directive_list.sgml"
+	for file in "$ROOT/sgml/"*; do
+		directive=${file##*/}
+	
+		echo "<link linkend=\"$directive\">$directive</link>" \
+			>>"$ROOT/output/directive_list.sgml"
+	done
+}
+
+rm -r "$ROOT/output"
+mkdir -p "$ROOT/output/linked/"
+
+build_by_context
+build_by_module
+build_by_name
+
+# Both jade(1) invocations exit with status 1, probably because of our
+# crappy SGML input. I hate blindly ignoring their exit status, but it's
+# easier than fixing the SGML. :-/
+jade -t sgml -V nochunks -E 100000 -d "$DSL" \
+	"$ROOT/configuration.sgml" \
+	>"$ROOT/output/configuration_full.html" || true
+
+cd "$ROOT/output/linked/"
+jade -t sgml -E 10000 -d "$DSL" "../../configuration.sgml" || true
